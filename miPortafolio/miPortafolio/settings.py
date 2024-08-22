4a60d9e2e5950d7b51fcb7a7724adb24238d5372
Lucosiar <lucosiar333@gmail.com>
2024-08-22 14:59:25 +0200

    Actualizando detalles

--- miPortafolio/miPortafolio/settings.py
+++ miPortafolio/miPortafolio/settings.py
@@ -142,6 +142,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATIC_ROOT = BASE_DIR / 'static/root'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
